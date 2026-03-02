7ed3791ed6e089765486673de226115e1e36e91a
Ilya Tupitsyn <wwwrt287@gmail.com>
2026-03-02 23:16:18 +0300

    Fix first lab

--- lab1.hs
+++ lab1.hs
@@ -1,36 +1,40 @@
 import Prelude hiding (sum)
 
 ak :: Integer -> Double -> Double -> Double
-ak n ap x = let
-    k = fromIntegral n
-    a = x ^ 2
-    b = 2 * k * (2 * k - 1)
-    in a / b * ap
+ak n ap x =
+  let k = fromIntegral n
+      a = x ^ 2
+      b = 2 * k * (2 * k - 1)
+   in a / b * ap
 
+ch x n ap s c
+  | n == c = s
+  | otherwise =
+      let nextA = ak c ap x
+       in ch x n nextA (s + nextA) (c + 1)
 
-ch x n ap s c e
-    | n == c = s
-    | otherwise = let
-        nextA = ak c ap x
-        sum = if abs nextA > e then s + nextA else s
-        in ch x n nextA sum (c+1) e
+che x ap s c e
+  | abs ap < e = s
+  | otherwise =
+      let nextA = ak c ap x
+       in che x nextA (s + nextA) (c + 1) e
 
-sum x n = ch x n 1 1 1 0
+sum x n = ch x n 1 1 1
 
-sumGrE x n = ch x n 1 1 1
+sumGrE x = che x 1 1 1
 
-sumGrE10 x n e = sumGrE x n (e / 10)  
+sumGrE10 x e = sumGrE x (e / 10)
 
 main = do
-    xStr <- getLine
-    nStr <- getLine
-    eStr <- getLine
+  xStr <- getLine
+  nStr <- getLine
+  eStr <- getLine
 
-    let x = read xStr :: Double
-        n = read nStr :: Integer
-        e = read eStr :: Double
-    
-    print $ sum x n
-    print $ sumGrE x n e
-    print $ sumGrE10 x n e
-    print $ cosh x
+  let x = read xStr :: Double
+      n = read nStr :: Integer
+      e = read eStr :: Double
+
+  print $ sum x n
+  print $ sumGrE x e
+  print $ sumGrE10 x e
+  print $ cosh x
